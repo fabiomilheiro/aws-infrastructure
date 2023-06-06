42fc0d17bda8bd1250b102774696d47a7026f30f
Fabio Milheiro <fabiomilheiro@gmail.com>
2023-06-07 00:59:58 +0100

    Create service1 and service2

---
+++ dotnet-services/Service1/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service1.ApiModels;
+
+namespace Service1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static UserApiModel[] Users = new[]
+        {
+            new UserApiModel{ Id = 1, Name = "John Smith" },
+            new UserApiModel{ Id = 2, Name = "Jane Smith" },
+            new UserApiModel{ Id = 3, Name = "James Smith" },
+            new UserApiModel{ Id = 4, Name = "Jim Smith" },
+            new UserApiModel{ Id = 5, Name = "Jim Smith" },
+        };
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return this.Ok(Users);
+        }
+    }
+}
