a85ae2a8d725ae44100bdb1ef5922ae0a8d630ec
Fabio Milheiro <fabiomilheiro@gmail.com>
2023-06-07 13:46:51 +0100

    Fix gitignores

---
+++ infrastructure/bin/app.ts
@@ -0,0 +1,69 @@
+import * as cdk from "aws-cdk-lib";
+import "source-map-support/register";
+import { BasicsStack } from "../lib/basicsStack";
+import { Service1Stack } from "../lib/service1Stack";
+import { Service2Stack } from "../lib/service2Stack";
+import {
+  EnvironmentName,
+  environmentValidator,
+  RegionName,
+  StackProps,
+} from "../lib/types";
+
+const environmentRegions: Record<EnvironmentName, RegionName> = {
+  dev: RegionName.EuropeWest1,
+  prod: RegionName.EuropeWest1,
+};
+
+const app = new cdk.App();
+
+const environmentName = environmentValidator.Parse(
+  process.env.ENVIRONMENT_NAME
+);
+
+const account = process.env.AWS_ACCOUNT;
+if (!account) {
+  throw new Error("AWS_ACCOUNT environment variable not defined.");
+}
+
+const defaultProps: StackProps = {
+  environmentName: environmentName,
+  buildNumber: process.env.BUILD_NUMBER ?? "(no build number)",
+  env: {
+    account: account,
+  },
+};
+
+const region = environmentRegions[environmentName];
+const generalStackName = `${region}-basics`;
+new BasicsStack(app, generalStackName, {
+  ...defaultProps,
+  env: {
+    account: account,
+    region: region,
+  },
+  stackName: generalStackName,
+  description: `Basic resources.`,
+});
+
+const service1StackName = `${region}-service1`;
+new Service1Stack(app, service1StackName, {
+  ...defaultProps,
+  env: {
+    account: account,
+    region: region,
+  },
+  stackName: service1StackName,
+  description: `Service 1 resources.`,
+});
+
+const service2StackName = `${region}-service2`;
+new Service2Stack(app, service2StackName, {
+  ...defaultProps,
+  env: {
+    account: account,
+    region: region,
+  },
+  stackName: service2StackName,
+  description: `Service 2 resources.`,
+});
---
+++ services/user/api/build/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+const aws_serverless_express_1 = __importDefault(require("aws-serverless-express"));
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/", (request, response) => {
+    console.log("Received request to /. Query string:", request.query);
+    response.send("Home");
+});
+app.get("/path/:name", (request, response) => {
+    console.log(`Received request to /path/${request.params.name}`);
+    response.send({
+        Message: "Response to your request",
+        baseUrl: request.baseUrl,
+        headers: request.headers,
+        originalUrl: request.originalUrl,
+        path: request.path,
+        params: request.params,
+    });
+});
+const userService = (0, express_1.default)();
+userService.use("/user", app);
+userService.listen(8000, () => {
+    console.log("Serving on port 8000.");
+});
+const server = aws_serverless_express_1.default.createServer(userService);
+const handler = (event, context) => {
+    aws_serverless_express_1.default.proxy(server, event, context);
+};
+exports.handler = handler;
